70362ba9f4ee3172d58aae45e4b3f8ef1859a116
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-12-27 11:20:27 -0800

    updated with weekend work

--- leetcode.js
+++ leetcode.js
@@ -432,3 +432,456 @@ function isSubsequence(s, t) {
     return i === s.length;
 
 }
+
+// input: path = "/a/./b/"
+//		 => ['a', ., 'b'] => result = pathOnly = ['a', 'b']
+//		 => a/b
+// output: "/a/b/"
+
+// input: path = "/a/././b/"
+// output: "/a/b/"
+
+// input: path = "/a/../b/" => [ 'a', .. , 'b'] => 
+// output: "/b/"
+
+// input: "/home//foo/" => [ 'home', '', 'foo'] => 
+//		=> ['home', 'foo']
+// output: "/home/foo/"
+
+// input: "/a/b/../../" => [ 'a', 'b', .. , ..]
+// output: "/"
+
+// split the string into parts using / 
+// create pathOnly = []
+// if it's '..' => add to a counter
+// take a slice of the pathOnly array and omit the last counter num of elements (ex. if there are 2 in the counter array - last 2 elements)
+// return the joined array of pathOnly
+
+// OPPORTUNITIES
+// Ask more clarifying questions at the beginning
+// Be sure to understand the problem statement
+// Edge case questions
+// Assumptions that I'm making
+// More test examples to make sure you understand the problem
+// Talk about my thinking and process more
+// Getting the basic functionality running
+// Space and time complexity
+// What data structures am I using
+// Refactoring after
+// Edge cases: Empty, Valid inputs, one input, 
+// Data structures
+// More practice 
+// Using Queues and Stacks for Front End
+// Optimizing Queues
+// Building things on the spot (vanilla JS or react)
+
+function dir(str) {
+    // your function here
+    let splitStr = str.split("/");
+    let pathOnly = []; // Stack
+    let counter = 0;
+
+    for (let elem of splitStr) {
+        if (elem === '..') {
+            pathOnly.pop();
+        } else if (elem !== '.' && elem !== '..') {
+            pathOnly.push(elem);
+        }
+    }
+
+    let result = pathOnly.slice(0, pathOnly.length - counter); // Not needed
+    result = result.join("/");
+
+    // 
+    return result.replaceAll("//", "/");
+}
+
+console.log(dir("/a/./b/"));
+console.log(dir("/a/././b/"));
+console.log(dir("/a/../b/"));
+console.log(dir("/home//foo/"));
+console.log(dir("/a/b/../../"));
+console.log(dir("/home//foo//hello/"));
+
+const assert = require('assert');
+
+function twoSum(nums, target) {
+    let seen = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let neededNum = target - nums[i];
+
+        if (seen[neededNum] !== undefined) return [seen[neededNum], i];
+        else seen[nums[i]] = i;
+    }
+}
+
+assert.deepEqual(twoSum([2, 7, 11, 15], 9), [0, 1]);
+assert.deepEqual(twoSum([3, 2, 4], 6), [1, 2]);
+assert.deepEqual(twoSum([3, 3], 6), [0, 1]);
+
+function isValid(str) {
+    let matchingChar = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
+
+    let checkStack = [];
+
+    for (let char of str) {
+        if (matchingChar[char]) checkStack.push(char);
+        else if (char === matchingChar[checkStack[checkStack.length - 1]]) {
+            checkStack.pop();
+        } else if (char !== matchingChar[checkStack[checkStack.length - 1]]) {
+            return false;
+        }
+    }
+    return checkStack.length === 0;
+}
+
+assert.equal(isValid("()"), true);
+assert.equal(isValid("()[]{}"), true);
+assert.equal(isValid("(]"), false);
+
+// function isSubsequence(s,t) {
+//   let sPointer = 0;
+//   let tPointer;
+
+//   for (tPointer = 0; tPointer < t.length; tPointer++) {
+//     if (s[sPointer] === t[tPointer]) {
+//       sPointer++;
+//     }
+//   }
+//   return sPointer === s.length;
+// }
+
+
+// assert.equal(isSubsequence("abc", "ahbgdc"), true);
+// assert.equal(isSubsequence("axc", "ahbgdc"), false);
+// assert.equal(isSubsequence("abc", "bca"), false);
+// assert.equal(isSubsequence("abc", "abdef"), false);
+
+// Create a max variable and set it to 0
+// Create a counter variable that starts at 0
+// find the min length of all three words
+// Loop through 0 to min. 
+// If all three words have that letter 
+
+// function longestCommonPrefix(strs){
+//   let common = strs[0][0];
+//   let i = 0;
+
+//   for (let str of strs) {
+
+//   }
+
+//   for (let j = 0; j < strs.length; j++) {
+//     if (
+//   }
+
+// }
+
+// assert.equal(longestCommonPrefix(["flower","flow","flight"]),"fl");
+// assert.equal(longestCommonPrefix(["dog","racecar","car"]), "");
+
+
+// canConstructWord
+// accepts 2 strings: a word and some letters
+// function should return true if the word can be built with the letters that are
+// you are given
+// otherwise return false
+
+function canConstructWord(word, letters) {
+    let lettersCounter = {};
+    let wordCounter = {};
+
+    for (let l of letters) {
+        lettersCounter[l] = (lettersCounter[l] || 0) + 1;
+    }
+
+    for (let w of word) {
+        wordCounter[w] = (wordCounter[w] || 0) + 1;
+    }
+
+    for (let letter in wordCounter) {
+        if (wordCounter[letter] > lettersCounter[letter]) return false;
+    }
+
+    return true;
+}
+
+
+assert.equal(canConstructWord('aa', 'abc'), false);
+assert.equal(canConstructWord('abc', 'dcba'), true);
+assert.equal(canConstructWord('aabbcc', 'bcabcaddff'), true);
+
+
+// averagePair
+// Given a sorted array of integers and a target average, determine if there is
+// a pair of values in the array where the average of the pair equals the target
+// average. There may be more than one pair that matches the average target
+
+function averagePair(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+
+    while (left < arr.length) {
+        let average = (arr[left] + arr[right]) / 2
+
+        if (average === target) return true;
+        if (average < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return false;
+}
+
+
+assert.equal(averagePair([1, 2, 3], 2.5), true);
+assert.equal(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8), true);
+assert.equal(averagePair([-1, 0, 3, 4, 5, 6], 4.1), false);
+assert.equal(averagePair([], 4), false);
+
+
+// twoArrayObject
+// Accepts 2 arrays of varying lengths
+// 1st array of keys, second array of values
+// return an object created from the keys and values
+// if not enough vales, the rest of keys should a value of null
+// if not enoug keys, ignore the rest of values
+
+assert.deepEqual(twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3]), { 'a': 1, 'b': 2, 'c': 3, 'd': null });
+assert.deepEqual(twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4]), { 'a': 1, 'b': 2, 'c': 3 });
+assert.deepEqual(twoArrayObject(['x', 'y', 'z'], [1, 2]), { 'x': 1, 'y': 2, 'z': null });
+
+function twoArrayObject(keys, values) {
+    let result = {};
+
+    for (let i = 0; i < keys.length; i++) {
+        result[keys[i]] = values[i] || null;
+    }
+
+    return result;
+}
+
+
+// sameFrequency
+// given two positive integers, find out if the two numbers have the same frequency of digits
+
+assert.equal(sameFrequency(182, 281), true);
+assert.equal(sameFrequency(34, 14), false);
+assert.equal(sameFrequency(3589578, 5879385), true);
+assert.equal(sameFrequency(22, 222), false);
+
+function frequencyCounter(str) {
+    let counter = {}
+
+    for (let char of str) {
+        counter[char] = (counter[char] || 0) + 1;
+    }
+
+    return counter;
+}
+
+function sameFrequency(num1, num2) {
+    let counter1 = frequencyCounter(num1.toString());
+    let counter2 = frequencyCounter(num2.toString());
+    return JSON.stringify(counter1) === JSON.stringify(counter2);
+}
+
+
+// SeparatePositive
+// accepts an array of non-zero integers
+// Separate the positive integers to the left and the negative integers to the right
+// positive numbers and negative numbers need not be in sorted order
+// done in place
+
+// assert.deepEqual(separatePositive([2, -1, -3, 6, -8, 10]), [2, 10, 6, -3, -1, -8]);
+// assert.deepEqual(separatePositive([5, 10, -15, 20, 25]), [5, 10, 25, 20, -15]);
+// assert.deepEqual(separatePositive([-5, 5]), [5,-5]);
+// assert.deepEqual(separatePositive([1, 2, 3]), [1, 2, 3]);
+
+function separatePositive(arr) {
+    let currIdx = 0;
+    // let swapIdx = 0;
+
+    for (let swapIdx = 0; swapIdx < arr.length; swapIdx++) {
+        // console.log({arr, currIdx, swapIdx});
+        if (arr[currIdx] > 0) {
+            currIdx++;
+            swapIdx = currIdx;
+        }
+        if (arr[swapIdx] > 0) {
+            [arr[currIdx], arr[swapIdx]] = [arr[swapIdx], arr[currIdx]];
+        }
+    }
+    // console.log("final return", arr);
+    return arr;
+}
+
+//isSubsequence
+// takes in two strings and checks whether the characters in the first string form a
+// subsequence of the characters in the second string
+
+assert.equal(isSubsequence('hello', 'hello world'), true);
+assert.equal(isSubsequence('sing', 'sting'), true);
+assert.equal(isSubsequence('abc', 'abracadabra'), true);
+assert.equal(isSubsequence('abc', 'acb'), false);
+
+function isSubsequence(str1, str2) {
+    let pointer1 = 0;
+    let pointer2 = 0;
+
+    while (pointer2 < str2.length) {
+        if (str1[pointer1] === str2[pointer2]) {
+            pointer1++;
+            pointer2++;
+        } else {
+            pointer2++;
+        }
+    }
+    // console.log({pointer1, pointer2, str1, str2});
+    return pointer1 === str1.length;
+}
+
+// countPairs
+// given an array of integers and a number, find the number of pairs of integers
+// in the the array whose sum is equal to the second parameter. You can assume
+// there there will be no duplicate values in the array
+
+assert.equal(countPairs([3, 1, 5, 4, 2], 6), 2);
+assert.equal(countPairs([10, 4, 8, 2, 6, 0], 10), 3) // 3 (2,8, 4,6, 10,0)
+assert.equal(countPairs([4, 6, 2, 7], 10), 1); // (4,6)
+assert.equal(countPairs([1, 2, 3, 4, 5], 10), 0);
+assert.equal(countPairs([1, 2, 3, 4, 5], -3), 0);
+assert.equal(countPairs([0, -4], -4), 1);
+assert.equal(countPairs([1, 2, 3, 0, -1, -2], 0), 2);
+
+function countPairs(arr, target) {
+
+    let count = 0;
+    let nums = new Set();
+
+    for (let num of arr) {
+        let neededNum = target - num;
+        if (nums.has(neededNum)) {
+            count++;
+        } else {
+            nums.add(num);
+        }
+    }
+    return count;
+}
+
+// longestFall
+// accepts an array of integers and returns the length of the longest consecutive decrease
+// of arrays
+
+assert.equal(longestFall([5, 3, 1, 3, 0]), 3);
+assert.equal(longestFall([2, 2, 1]), 2);
+assert.equal(longestFall([2, 2, 2]), 1);
+assert.equal(longestFall([5, 4, 4, 4, 3, 2]), 3);
+assert.equal(longestFall([9, 8, 7, 6, 5, 6, 4, 2, 1]), 5);
+assert.equal(longestFall([]), 0);
+
+// Create a counter starting at 0;
+// Create a longest variable starting at 0;
+// Let currNum = +Infinity;
+// loop through the array and adding to the counter if the number is less than currNum
+// make the number into the currNum.
+// If the number is equal or greater than currNum, compare the current counter to the longest
+// variable and update the longest to the larger of the two
+// reset the counter to 0;
+
+function longestFall(nums) {
+    let longest = 0;
+    let counter = 0;
+    let currNum = +Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        // console.log({i, currNum, nums, longest,counter});
+        if (nums[i] < currNum) {
+            counter += 1;
+            currNum = nums[i];
+        } else {
+
+            counter = 0;
+            currNum = +Infinity;
+            i--
+        }
+        longest = Math.max(longest, counter);
+        // console.log("end", {i, currNum, nums, longest,counter});
+    }
+
+    return longest;
+}
+
+
+// PivotIndex
+// Accepts an array of integers and returns the pivot index where the sum of the items
+// on the left equal to the sum of the items on the right
+// if there are more than one valid pivot index, return the smallest value
+
+assert.equal(pivotIndex([1, 2, 1, 6, 3, 1]), 3);
+assert.equal(pivotIndex([5, 2, 7]), -1);
+assert.equal(pivotIndex([-1, 3, -3, 2]), 1);
+assert.equal(pivotIndex([]), -1);
+
+// Starting at index 1, loop through the array
+// left side = arr[1] and right = sum of the remaining nums excluding the pivot point
+// if left !== right, add the pivot num to left, subtract the first number of the right side
+// check again
+// if left reaches the end of the array, then return -1
+
+function pivotIndex(nums) {
+    // if (nums.length === 0) return -1;
+
+    let left = nums[0];
+    let right = nums.reduce((a, b) => a + b, 0) - left - nums[1];
+    let pivot = 1;
+
+    while (left < nums.length) {
+        if (left === right) return pivot;
+        else {
+            left += nums[pivot];
+            pivot += 1;
+            right -= nums[pivot];
+        }
+    }
+    return -1;
+}
+
+
+// longest commonPrefix
+
+// given an array of strings,f ind the longest common prefix string
+// if there is no common prefix return ''
+
+assert.equal(longestCommonPrefix(["flower", "flow", "flight"]), 'fl');
+assert.equal(longestCommonPrefix(["dog", "racecar", "car"]), '');
+
+
+// Create a result variable '';
+// loop through each word in the array. If the first letter of the array === add it to the 
+// result variable
+
+function longestCommonPrefix(strs) {
+    let result = '';
+    let shortestLength = Math.min(...strs.map(str => str.length));
+
+
+    for (let i = 0; i < shortestLength; i++) {
+        let currChar = strs[0][i];
+
+        if (strs.every(str => str[i] === currChar)) {
+            result += currChar;
+        } else {
+            break;
+        }
+    }
+    return result;
+}

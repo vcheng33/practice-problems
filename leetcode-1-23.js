3e2a022cf5205e5b60ac9a6b0711d0d700230763
vcheng33 <vanessa.ah.cheng@gmail.com>
2022-01-24 18:03:28 -0800

    practices from today

--- leetcode-1-23.js
+++ leetcode-1-23.js
@@ -314,3 +314,261 @@ function converter(str) {
 
     return `${maxLetter} ${maxCount}`;
 }
+
+const assert = require('assert');
+
+assert.equal(reverse("Let's take LeetCode contest"), "s'teL ekat edoCteeL tsetnoc");
+assert.equal(reverse("God Ding"), "doG gniD");
+
+function reverse(str) {
+
+    let words = str.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        let wordArr = words[i].split("");
+        for (let j = 0; j < wordArr.length / 2; j++) {
+            [wordArr[j], wordArr[wordArr.length - 1 - j]] = [wordArr[wordArr.length - 1 - j], wordArr[j]];
+            // console.log({wordArr}, wordArr[j], wordArr[wordArr.length - 1 - j]);
+        }
+        // console.log({wordArr});
+        words[i] = wordArr.join("");
+    }
+
+    return words.join(" ");
+}
+
+function reverse(str) {
+    let reversedStr = '';
+    let currWord = '';
+
+    for (let char of str) {
+        if (char === " ") {
+            currWord += char;
+            reversedStr += currWord;
+            currWord = '';
+        } else {
+            currWord = char + currWord;
+            console.log({ currWord })
+        }
+    }
+
+    reversedStr += currWord;
+
+    return reversedStr;
+}
+
+
+/*
+  Args:
+    pace: a number, the number of minutes it takes you per road.
+    roads: a list of list of strings, ex [["F","A"],["A","B"],["B","Y"],["Y","H"]] 
+           each list is the start/end of a road
+           ex: ["A", "B"] is a road from A to B (roads are all two-way)
+  Returns:
+    A string, either "YOU WIN" if you make it home first, or "FRIEND WINS" if
+    your friend makes it to your house first (or if it is a tie)
+*/
+
+function raceWinner(pace, roads) {
+    let roadsObj = {};
+    roads.forEach(r => {
+        if (roadsObj[r[0]] === undefined) {
+            roadsObj[r[0]] = [r[1]];
+        } else if (roadsObj[r[0]]) {
+            roadsObj[r[0]].push(r[1]);
+        }
+
+        if (roadsObj[r[1]] === undefined) {
+            roadsObj[r[1]] = [r[0]];
+        } else if (roadsObj[r[1]]) {
+            roadsObj[r[1]].push(r[0]);
+        }
+    });
+
+    let youTime = timeToHome(pace, roadsObj, 'Y');
+    let friendTime = timeToHome(1, roadsObj, 'F');
+
+    return friendTime <= youTime * pace ? "FRIEND WINS" : "YOU WIN";
+}
+
+/** 
+
+
+*/
+function timeToHome(pace, roadsObj, startLetter) {
+
+    let visited = new Set();
+    visited.add(startLetter);
+    let toVisitQueue = [[startLetter, 0]];
+
+
+    while (toVisitQueue.length) {
+        let [start, time] = toVisitQueue.shift();
+
+
+        let options = roadsObj[start];
+
+        if (options === undefined) return time;
+        if (options.includes('H')) return time + 1;
+        else {
+            for (let i = 0; i < options.length; i++) {
+
+                if (!visited.has(options[i])) {
+                    visited.add(options[i]);
+                    toVisitQueue.push([options[i], time + 1]);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DO NOT MODIFY BELOW THIS LINE
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+});
+
+var pace = -1;
+var roads = [];
+var break_line = false;
+
+rl.on('line', (line) => {
+    line = line.trim();
+    if (line == '') {
+        break_line = true;
+        return;
+    }
+    if (break_line) {
+        road = line.split(' ');
+        roads.push(road);
+    } else {
+        pace = parseInt(line, 10);
+    }
+}).on('close', () => {
+    console.log(raceWinner(pace, roads));
+});
+
+/*
+  Args:
+    pace: a number, the number of minutes it takes you per road.
+    roads: a list of list of strings, ex [["F","A"],["A","B"],["B","Y"],["Y","H"]] 
+           each list is the start/end of a road
+           ex: ["A", "B"] is a road from A to B (roads are all two-way)
+  Returns:
+    A string, either "YOU WIN" if you make it home first, or "FRIEND WINS" if
+    your friend makes it to your house first (or if it is a tie)
+*/
+
+function raceWinner(pace, roads) {
+    let roadsObj = createRoadsObj(roads);
+
+    let youBlocks = timeToHome(roadsObj, 'Y');
+    let friendBlocks = timeToHome(roadsObj, 'F');
+
+    return friendBlocks <= youBlocks * pace ? "FRIEND WINS" : "YOU WIN";
+}
+
+/*
+  Args:
+    roads: a list of list of strings, ex [["F","A"],["A","B"],["B","Y"],["Y","H"]] 
+           each list is the start/end of a road
+           ex: ["A", "B"] is a road from A to B (roads are all two-way)
+  Returns:
+    An object with all of the roads from a point 
+    ex. { 'F' : ['A', 'B'], 'A' : ['B', 'F'], 'B': ['Y', 'A'], 'Y' : ['H', 'B'], 'H' : ['Y']}
+*/
+
+function createRoadsObj(roads) {
+    let roadsObj = {};
+
+    for (let [start, end] of roads) {
+        if (roadsObj[start] === undefined) roadsObj[start] = [end];
+        else roadsObj[start].push(end);
+
+        if (roadsObj[end] === undefined) roadsObj[end] = [start];
+        else roadsObj[end].push(start);
+    }
+
+    return roadsObj;
+}
+
+
+/** A breadth first search that starts at a person's letter (ex. 'Y' or 'F') and returns the    
+    shortest number of blocks to 'H'.
+    
+    Args:
+      roadsObj: an object with each starting point (ex. {'A' : 'B', 'B': 'C', 'C': 'H'})
+      startingPoint: the point to start the search
+   
+    Returns:
+      A number that represents the shortest number of blocks to get to 'H'
+*/
+function timeToHome(roadsObj, startingPoint) {
+
+    let visited = new Set(startingPoint);
+    let toVisitQueue = [[startingPoint, 0]];
+
+    while (toVisitQueue.length) {
+        let [start, numBlocks] = toVisitQueue.shift();
+
+        let possiblePointsFromStart = roadsObj[start];
+
+        for (let point of possiblePointsFromStart) {
+            if (point === 'H') return numBlocks + 1;
+
+            if (!visited.has(point)) {
+                visited.add(point);
+                toVisitQueue.push([point, numBlocks + 1]);
+            }
+        }
+    }
+}
+
+
+
+
+
+// DO NOT MODIFY BELOW THIS LINE
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+});
+
+var pace = -1;
+var roads = [];
+var break_line = false;
+
+rl.on('line', (line) => {
+    line = line.trim();
+    if (line == '') {
+        break_line = true;
+        return;
+    }
+    if (break_line) {
+        road = line.split(' ');
+        roads.push(road);
+    } else {
+        pace = parseInt(line, 10);
+    }
+}).on('close', () => {
+    console.log(raceWinner(pace, roads));
+});
